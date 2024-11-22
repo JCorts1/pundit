d88eed11db7ce1e0e36fc7d0795ce8e632874a76
Jay Cortes <cortesjay1991@gmail.com>
2024-11-22 17:08:04 +0100

    backend is done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
+  skip_after_action :verify_policy_scoped, only: [:home] # Skip Pundit check for home action
 
   def home
   end
